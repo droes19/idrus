ec77224aa7029850f6ddfccee0d8255b4652e5bd
idrus.kaafi <idrus^kaafi@bcadigital.co.id>
2024-01-12 21:19:29 +0700

    Auto-generated commit [ci skip]

---
+++ browser/chunk-EI355SEV.js
@@ -0,0 +1,1 @@
+import{Ba as s,R as o,sa as i,ta as a,wa as r}from"./chunk-GU2HDTFC.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-home"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"home works!"),a())}});let t=e;return t})();export{d as HomeComponent};
